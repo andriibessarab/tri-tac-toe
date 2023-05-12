9b8a5bdde5d25d57d84541f1d95cb9c03c8c54be
andriibessarab <andriibessarab@gmail.com>
2023-05-12 14:16:33 -0400

    Move contents of ./server into root directory

--- server/app/app_utils/validation_patterns.py
+++
@@ -1,6 +0,0 @@
-class ValidationPatterns:
-    # Regex patterns for username, email, password, and join code validation
-    USERNAME_PATTERN = r"^[a-zA-Z0-9_-]{3,16}$"
-    EMAIL_PATTERN = r"^[\w-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$"
-    PASSWORD_PATTERN = r"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$"
-    JOIN_CODE_PATTERN = r"^\d{6}$"
--- server/run.py
+++
@@ -1,7 +0,0 @@
-from app.__init__ import create_app
-
-
-if __name__ == '__main__':
-    app, socket = create_app()
-
-    socket.run(app, debug=True, host="0.0.0.0", port=5001)
