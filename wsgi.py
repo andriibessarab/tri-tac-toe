05cfa09d23ec81b109ffa727dd9eced7630026e3
andriibessarab <andriibessarab@gmail.com>
2023-05-12 16:24:35 -0400

    Modify procfile and local run method

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,10 @@
+from eventlet import wsgi
+import eventlet
+
 from server.__init__ import create_app
 
 app, socket = create_app()
 
 if __name__ == '__main__':
-    socket.run(app, debug=True, host="0.0.0.0", port=5001)
+    eventlet_socket = eventlet.listen(("", 5001))
+    wsgi.server(eventlet_socket, app)
