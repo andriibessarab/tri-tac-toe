86256ac9bfe6c676b90ab364e311ebe38a6176f6
andriibessarab <andriibessarab@gmail.com>
2023-05-13 15:34:57 -0400

    modify development server to take environmental variables from .env

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,12 @@
-from eventlet import wsgi
 import eventlet
+from dotenv import load_dotenv
+from eventlet import wsgi
 
 from server.__init__ import create_app
 
 app, socket = create_app()
 
 if __name__ == '__main__':
+    load_dotenv()  # take environment variables from .env
     eventlet_socket = eventlet.listen(("", 5001))
     wsgi.server(eventlet_socket, app)
