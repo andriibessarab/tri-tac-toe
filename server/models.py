553cedcba64a2c6d71a5eba0f6db1a25069b880e
andriibessarab <andriibessarab@gmail.com>
2023-05-13 16:27:22 -0400

    Make game model

--- server/models.py
+++ server/models.py
@@ -12,14 +12,7 @@ class User(db.Model):
     created_at = db.Column("created_at", db.DateTime, nullable=False, default=db.func.now())
 
     def to_json(self):
-        return {
-            "id": self.id,
-            "username": self.username,
-            "email": self.email,
-            "password": self.password,
-            "user_role": self.user_role,
-            "created_at": self.created_at,
-        }
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
     def is_authenticated(self):
         return True
@@ -36,3 +29,46 @@ class User(db.Model):
     def __repr__(self):
         return f"<User(id={self.id}, username='{self.username}', email='{self.email}')>"
 
+
+class Game(db.Model):
+    __tablename__ = "game"
+
+    id = db.Column("id", db.Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+    game_mode = db.Column(db.String, nullable=False, unique=False)
+    join_code = db.Column(db.String, nullable=True, unique=True)
+    player_1 = db.Column(db.Integer, nullable=False, unique=False, db.ForeignKey("user.id"))
+    player_2 = db.Column(db.Integer, nullable=True, unique=False, db.ForeignKey("user.id"))
+    player_1_marker = db.Column(db.Enum('x', 'o'), nullable=False, unique=False)
+    player_2_marker = db.Column(db.Enum('x', 'o'), nullable=False, unique=False)
+    winner = db.Column(db.Integer, nullable=True, unique=False, db.ForeignKey("user.id"))
+    created_at = db.Column(db.TIMESTAMP, nullable=False, server_default=db.func.now())
+
+    def to_json(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+    def get_room_id(self):
+        return f"game-{self.id}"
+
+    def get_players(self):
+        players = User.query.filter(User.id.in_([self.player_1, self.player_2])).all()
+        player_dict = {player.id: player.to_json() for player in players}
+        return {
+            "player_1": player_dict.get(self.player_1),
+            "player_2": player_dict.get(self.player_2),
+        }
+
+    def get_winner(self):
+        if self.winner:
+            winner = User.query.get(self.winner)
+            return winner.to_json() if winner else None
+        else:
+            return None
+
+    def is_full(self):
+        return self.get_players_count() == 2
+
+    def is_completed(self):
+        return self.winner is not None
+
+    def __repr__(self):
+        return f"<Game(id={self.id}, join_code='{self.join_code}')>"
